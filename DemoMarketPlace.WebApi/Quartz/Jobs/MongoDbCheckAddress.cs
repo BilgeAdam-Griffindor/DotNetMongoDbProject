6ab29e5bbe5ffe697df362e47d8d121c8dfd5586
Recai-Capkin <recai.capkin28@gmail.com>
2022-09-30 00:30:21 +0300

    Quartz işlemleri yapıldı.

---
+++ DemoMarketPlace.WebApi/Quartz/Jobs/MongoDbCheckAddress.cs
@@ -0,0 +1,18 @@
+﻿using Quartz;
+
+namespace DemoMarketPlace.WebApi.Quartz.Jobs
+{
+    public class MongoDbCheckAddress:IJob
+    {
+        public MongoDbCheckAddress()
+        {
+
+        }
+        public Task Execute(IJobExecutionContext context)
+        {
+            //Todo Yapılacak İş
+            System.Console.WriteLine("Evet Quart çalıştı.....");
+            return Task.CompletedTask;
+        }
+    }
+}

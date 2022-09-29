135920f659d5bad1687b7a003a36d64b54c17815
Essrinc <sabireesraozcan@gmail.com>
2022-09-29 23:26:55 +0300

    test

---
+++ DemoMarketPlace.Mvc/Controllers/CategoryController.cs
@@ -0,0 +1,48 @@
+﻿using DemoMarketPlace.Mvc.ApiService;
+using DemoMarketPlace.Mvc.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DemoMarketPlace.Mvc.Controllers
+{
+    public class CategoryController:Controller
+    {
+        private readonly DemoMarketApiService demoMarketApiService;
+        public CategoryController(DemoMarketApiService demoMarketApiService)
+        {
+            this.demoMarketApiService = demoMarketApiService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await demoMarketApiService.GetAllCategoryAsync());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Save() 
+        {
+            CategoryAddDTO categoryAddDTO = new CategoryAddDTO();
+            var categoriesDto = await demoMarketApiService.SaveCategoryAsync(categoryAddDTO);
+
+            ViewBag.Categories = new SelectList("Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(CategoryAddDTO categoryAddDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                await demoMarketApiService.SaveCategoryAsync(categoryAddDTO);
+                return RedirectToAction("Index");
+            }
+
+            var categoriesDto = await demoMarketApiService.GetAllCategoryAsync();
+            ViewBag.Categories = new SelectList(categoriesDto, "Id", "Name");
+
+            return View();
+        }
+
+    }
+}
---
+++ DemoMarketPlace.Mvc/DTO/CategoryListDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoMarketPlace.Mvc.DTO
+{
+    public class CategoryListDTO
+    {
+        public int CategoryID { get; set; }
+        public string? CategoryName { get; set; }
+    }
+}
